78fbdcacd711945e6fb342d2ebcd4f1c456bc6d6
Joseph Harburg <josephharburg@gmail.com>
2019-10-24 21:33:20 -0400

    inital program

--- Lab7JosephHarburg/Program.cs
+++ Lab7JosephHarburg/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace Lab7JosephHarburg
 {
@@ -6,7 +7,52 @@ namespace Lab7JosephHarburg
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Name();
+            //Email();
+            Phone();
+        }
+        public static void Name()
+        {
+            Console.WriteLine("Please enter your name!");
+            string nameInput = Console.ReadLine();
+            Match nameValidate = Regex.Match(nameInput, "(^[A-Z][a-z]{1,30})");
+            if (nameValidate.Success)
+            {
+                Console.WriteLine($"Hello {nameInput}");
+            }
+            else
+            {
+                Console.WriteLine("Im sorry thats not a valid name!");
+            }
+        }
+
+        public static void Email()
+        {
+            Console.WriteLine("Please enter your email!");
+            string emailInput = Console.ReadLine();
+            Match emailValidate = Regex.Match(emailInput, "([A-Za-z0-9]{5,30}@{1}[A-Za-z0-9]{5,10}.{1}[A-Za-z0-9]{2,3}$)");
+            if (emailValidate.Success)
+            {
+                Console.WriteLine($"Your Email is {emailInput}");
+            }
+            else
+            {
+                Console.WriteLine("Im sorry thats not a valid email!");
+            }
+        }
+        public static void Phone()
+        {
+            Console.WriteLine("Please enter your Phone Number!");
+            string phoneInput = Console.ReadLine();
+            Match phoneValidate = Regex.Match(phoneInput, @"(^(\d\d\d)-(\d\d\d)-(\d\d\d\d)$)");
+            if (phoneValidate.Success)
+            {
+                Console.WriteLine($"Your Email is {phoneInput}");
+            }
+            else
+            {
+                Console.WriteLine("Im sorry thats not a valid phone number!");
+            }
         }
     }
 }
