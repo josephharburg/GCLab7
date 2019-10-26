c93966d955d776e26310a5d4a0d0d5442df11523
Joseph Harburg <josephharburg@gmail.com>
2019-10-26 19:40:36 -0400

    Valdiations complete

--- Lab7JosephHarburg/Program.cs
+++ Lab7JosephHarburg/Program.cs
@@ -7,9 +7,10 @@ namespace Lab7JosephHarburg
     {
         static void Main(string[] args)
         {
-            //Name();
-            //Email();
+            Name();
+            Email();
             Phone();
+            Date();
         }
         public static void Name()
         {
@@ -18,7 +19,7 @@ namespace Lab7JosephHarburg
             Match nameValidate = Regex.Match(nameInput, "(^[A-Z][a-z]{1,30})");
             if (nameValidate.Success)
             {
-                Console.WriteLine($"Hello {nameInput}");
+                Console.WriteLine($"Hello {nameInput}!");
             }
             else
             {
@@ -30,10 +31,10 @@ namespace Lab7JosephHarburg
         {
             Console.WriteLine("Please enter your email!");
             string emailInput = Console.ReadLine();
-            Match emailValidate = Regex.Match(emailInput, "([A-Za-z0-9]{5,30}@{1}[A-Za-z0-9]{5,10}.{1}[A-Za-z0-9]{2,3}$)");
+            Match emailValidate = Regex.Match(emailInput, @"([A-Za-z0-9]{5,30}@{1}[A-Za-z0-9]{5,10}\.{1}[A-Za-z0-9]{2,3}$)");
             if (emailValidate.Success)
             {
-                Console.WriteLine($"Your Email is {emailInput}");
+                Console.WriteLine($"Your Email is {emailInput}!");
             }
             else
             {
@@ -47,11 +48,28 @@ namespace Lab7JosephHarburg
             Match phoneValidate = Regex.Match(phoneInput, @"(^(\d\d\d)-(\d\d\d)-(\d\d\d\d)$)");
             if (phoneValidate.Success)
             {
-                Console.WriteLine($"Your Email is {phoneInput}");
+                Console.WriteLine($"Your Phone Number is {phoneInput}!");
             }
             else
             {
                 Console.WriteLine("Im sorry thats not a valid phone number!");
+            }
+        }
+
+        public static void Date()
+        {
+            Console.WriteLine("Please enter a date!");
+            string dateInput = Console.ReadLine();
+            Match dateValidate = Regex.Match(dateInput, @"(^\d\d/{1}\d\d/{1}\d\d\d\d$)");
+            bool isDate = DateTime.TryParse(dateInput, out DateTime Datevalidated);
+
+            if (dateValidate.Success && isDate == true)
+            {
+                Console.WriteLine($"The date you wrote is {dateInput}!");
+            }
+            else
+            {
+                Console.WriteLine("Im sorry thats not a valid date!");
             }
         }
     }
